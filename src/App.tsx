696f2352eb2808a3312394cb547483563215b29a
Andrey35211 <serand3521@yandex.ru>
2021-10-19 12:10:49 +0700

    feat: test lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+// eslint-disable-next-line
 function App() {
-  // eslint-disable-line
   return (
     <div className="App">
       <header className="App-header">
