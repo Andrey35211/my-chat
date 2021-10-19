928a32ca8607284e6a6b6016dcf4e3a2e3c96a2f
Andrey35211 <serand3521@yandex.ru>
2021-10-19 12:08:58 +0700

    feat: test lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,20 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-var string = "<p>gffgh</p>"
-
 function App() {
   // eslint-disable-line
   return (
     <div className="App">
       <header className="App-header">
-             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-             </p>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-                </p>
+        </p>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
         {/* <a
-          className="App-link"
+          clagitssName="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopene    r noreferrer"
